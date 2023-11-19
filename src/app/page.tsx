2134f88df24db36d0036d23a8cf170213d8e266b
CedricAngeloSina-on <cao.sinaon@gmail.com>
2023-11-19 13:19:53 +0800

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
-
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
+
 export default function Home() {
     return (
         <main className="pt-24 lg:w-1/2 lg:py-24">
