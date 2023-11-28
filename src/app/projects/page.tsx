b273c6ab86ce1f9ee146d0dae93f7192df51c089
CedricAngeloSina-on <cao.sinaon@gmail.com>
2023-11-28 22:59:29 +0800

    added supabase
    added supabase for the projects table to overcomplicated things

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,72 +1,43 @@
 import React from "react";
 import Link from "next/link";
 import ProjectTableRow from "../components/ProjectTableRow";
+import supabase from "../utils/supabase";
 
-export default function Projects() {
-    const projectRowItems = [
-        {
-            title: "Personal Website",
-            company: "",
-            technologies: ["Nextjs", "React", "Vercel", "Tailwind CSS", "HTML"],
-            linktext: "cedricsina-on.vercel.app",
-            href: "https://cedricsina-on.vercel.app",
-        },
-        {
-            title: "Netbeans Activity Point of Sale",
-            company: "",
-            technologies: ["Java"],
-            linktext: "Github",
-            href: "https://github.com/CedricAngeloSina-on/java-netbeans-activity",
-        },
-        {
-            title: "Bocchi the Rock Cafe Menu!",
-            company: "",
-            technologies: ["HTML", "CSS", "Javascript"],
-            linktext: "bocchimenu.vercel.app",
-            href: "https://bocchimenu.vercel.app",
-        },
-        {
-            title: "Steins;Gate Fan-Made Site (Immersion Project)",
-            company: "Maven Solutions Inc.",
-            technologies: ["HTML", "CSS", "Javascript"],
-            linktext: "sgfansite.vercel.app",
-            href: "https://sgfansite.vercel.app",
-        },
-        {
-            title: "Sapatos ni Danyl (Immersion Project)",
-            company: "Maven Solutions Inc.",
-            technologies: ["HTML", "CSS", "Javascript"],
-            linktext: "sapatos-ni-danyl.vercel.app",
-            href: "https://sapatos-ni-danyl.vercel.app",
-        },
-    ];
+export const revalidate = 0;
+
+export default async function Projects() {
+    const { data } = await supabase.from("projects").select().order("id");
+    const projectRowItems = data;
 
     return (
         <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
             <div className="lg:py-24">
-                <Link
-                    className="group mb-2 inline-flex items-center font-semibold leading-tight text-sky-400"
-                    href="/"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="mr-1 h-4 w-4 rotate-180 transition-transform group-hover:-translate-x-2"
+                <header>
+                    <Link
+                        className="group mb-2 inline-flex items-center font-semibold leading-tight text-sky-400"
+                        href="/"
                     >
-                        <path
-                            fillRule="evenodd"
-                            d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
-                            clipRule="evenodd"
-                        ></path>
-                    </svg>
-                    Cedric Sina-on
-                </Link>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="mr-1 h-4 w-4 rotate-180 transition-transform group-hover:-translate-x-2"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
+                                clipRule="evenodd"
+                            ></path>
+                        </svg>
+                        Cedric Sina-on
+                    </Link>
+                </header>
+
                 <h1 className="font-extrabold tracking-tight text-white xs:text-4xl sm:text-5xl">
                     All Projects
                 </h1>
                 <table className="mt-12 w-full border-collapse text-left">
-                    <thead className="sticky top-0 z-10 border-b border-white/10 px-6 py-5">
+                    <thead className="border-b border-white/10 px-6 py-5">
                         <tr>
                             <th className="py-4 pr-8 text-base font-semibold text-white">
                                 Project
@@ -83,14 +54,14 @@ export default function Projects() {
                         </tr>
                     </thead>
                     <tbody>
-                        {projectRowItems.map((projectRowItem, index) => (
+                        {projectRowItems?.map((projectItem) => (
                             <ProjectTableRow
-                                key={index}
-                                title={projectRowItem.title}
-                                company={projectRowItem.company}
-                                technologies={projectRowItem.technologies}
-                                linktext={projectRowItem.linktext}
-                                href={projectRowItem.href}
+                                key={projectItem.id}
+                                title={projectItem.title}
+                                company={projectItem.company}
+                                technologies={projectItem.technologies}
+                                linktext={projectItem.linktext}
+                                href={projectItem.href}
                             />
                         ))}
                     </tbody>
