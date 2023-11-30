443a7d1b5611b22ba3204e7aee634e9bd07bdf0d
CedricAngeloSina-on <cao.sinaon@gmail.com>
2023-11-30 14:45:42 +0800

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import Spotlight from "./components/Spotlight";
@@ -18,6 +19,18 @@ export default function RootLayout({
             lang="en"
             className={`scroll-smooth bg-zinc-900 ${GeistSans.className}`}
         >
+            <Head>
+                <meta property="og:image" content="/images/homepage.jpg" />
+                <meta
+                    property="og:image:alt"
+                    content="Cedric Angelo Sina-on - Portfolio"
+                />
+                <meta name="twitter:image" content="/images/homepage.jpg" />
+                <meta
+                    property="twitter:image:alt"
+                    content="Cedric Angelo Sina-on - Portfolio"
+                />
+            </Head>
             <body>
                 <Spotlight />
                 {children}
